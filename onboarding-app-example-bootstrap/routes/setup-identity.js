7d8972919c6bce4ddbd215f5d6b0fb8aeda796fe
Frederick Pearson <fpearson@cloudstrucc.com>
2025-03-24 12:55:01 -0400

    support for redeem invitation

--- onboarding-app-example-bootstrap/routes/setup-identity.js
+++ onboarding-app-example-bootstrap/routes/setup-identity.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // GET route for '/newpage'
-router.get('/setup-identity', (req, res) => {
+router.use('/setup-identity', (req, res) => {
   res.render('setup-identity', { title: 'Setup identity' });
 });
 
