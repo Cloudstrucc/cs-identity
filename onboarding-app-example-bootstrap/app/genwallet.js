f4d7141d2732f3cfa76a1a3fbe57d38e87d25e26
Frederick Pearson <fpearson@cloudstrucc.com>
2025-03-24 06:19:12 -0400

    updated readme and azure steps

--- onboarding-app-example-bootstrap/app/genwallet.js
+++
@@ -1,7 +0,0 @@
-const { Wallet } = require("ethers");
-
-// Generate a new random wallet
-const wallet = Wallet.createRandom();
-
-console.log("Ethereum_Address:", wallet.address);
-console.log("Private_Key:", wallet.privateKey);
